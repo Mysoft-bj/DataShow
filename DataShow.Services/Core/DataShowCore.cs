5f26c09321faa127cfcdd9aee7b4015f07090829
yangmy <504623635@qq.com>
2018-01-22 00:37:27 +0800

    解决方案整理

---
+++ DataShow.Services/Core/DataShowCore.cs
@@ -0,0 +1,33 @@
+﻿using My.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataShow.Core.Services
+{
+    public class DataShowCore
+    {
+        public void CreateDataShowAllJson(Guid EntitGUID) {
+
+        }
+
+
+        public void CreateDataShowPageJson(Guid EntityPageGUID)
+        {
+            
+
+        }
+
+        public void CreateDataModelJsonTree(List<DsEntityPageNodeData> PageList)
+        {
+            var maxLevel = PageList.Max(page => page.Level);
+
+
+
+        }
+    }
+
+
+}
