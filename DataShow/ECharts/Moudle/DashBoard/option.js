de94f86a0dc27a5d7d60b5e9fdabea9c7c171942
unknown <yaoxl@mysoft.com.cn>
2018-02-07 11:58:41 +0800

    Revert "Merge branch 'Branch1' of https://github.com/Mysoft-bj/DataShow into Branch1"
    This reverts commit 7b917f749f529dd55931680ebca52bad7ecc4cad, reversing
    changes made to 3a1fd0640c821aa9fe928b8fe1609c54bc80f989.

--- DataShow/ECharts/Moudle/DashBoard/option.js
+++ DataShow/ECharts/Moudle/DashBoard/option.js
@@ -4,7 +4,7 @@ function CreateDashBoard(divId, jsonData) {
         backgroundColor: "", //容器背景色，为空背景透明
         formatvalue: "%", //格式化显示配置,会加在数据显示后面
         color: "#b2975a", //仪表盘及标题显示颜色
-        width: "5", //表盘弧形宽度
+        width: "10", //表盘弧形宽度
         datavale: "50", //数据值
         dataname: "XX"//显示名称
     }
@@ -17,13 +17,6 @@ function CreateDashBoard(divId, jsonData) {
             detail: {
                 formatter: '{value}' + newData.formatvalue
             },
-            title : {
-                textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-                    fontWeight: 'bolder',
-                    fontSize: 20,
-                    fontStyle: 'italic'
-                }
-            },
             axisLine: {
                 show: true,
                 lineStyle: {
@@ -32,18 +25,6 @@ function CreateDashBoard(divId, jsonData) {
                     color: [
                         [1, newData.color == "" ? defaultOption.color : newData.color]
                     ]
-                }
-            },
-            axisTick: {            // 坐标轴小标记
-                length: 8,        // 属性length控制线长
-                lineStyle: {       // 属性lineStyle控制线条样式
-                    color: 'auto'
-                }
-            },
-            splitLine: {           // 分隔线
-                length: 14,         // 属性length控制线长
-                lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
-                    color: 'auto'
                 }
             },
             data: [{
---
+++ DataShow/js/my.js
@@ -0,0 +1,135 @@
+﻿(function () {
+    var RoutePath = "/ApiService/Execute",
+    
+    regGet = new RegExp(/^get/);
+    regDelete = new RegExp(/^delete/);
+    var my = {};
+    my.log = function (msg) {
+        console && console.log && console.log(msg);
+    }
+    my.info = function (msg) {
+        alert(msg);
+    };
+    my.confirm = function (msg) {
+        return confirm(msg);
+    };
+    my.success = function (msg) {
+        alert(msg);
+    };
+    my.error = function (msg) {
+        alert(msg);
+    }
+    my.getJsonName = function (json) {
+        var obj = eval('(' + json + ')');
+        var sRtn = "[";
+        for (var i = 0; i < obj.length; i++) {
+            if (i == obj.length - 1) {
+                sRtn += "'" + obj[i].name + "'";
+            }
+            else {
+                sRtn += "'" + obj[i].name + "',";
+            }
+        }
+        sRtn += "]";
+        return eval('(' + sRtn + ')');
+    }
+    my.getJsonValue = function (json) {
+        var obj = eval('(' + json + ')');
+        var sRtn = "[";
+        for (var i = 0; i < obj.length; i++) {
+            if (i == obj.length - 1) {
+                sRtn += "" + obj[i].value + "";
+            }
+            else {
+                sRtn += "" + obj[i].value + ",";
+            }
+        }
+        sRtn += "]";
+        return eval('(' + sRtn + ')');
+    }
+    my.getJsonNameByJosn = function (json) {
+        var obj = json;
+        var sRtn = "[";
+        for (var i = 0; i < obj.length; i++) {
+            if (i == obj.length - 1) {
+                sRtn += "'" + obj[i].name + "'";
+            }
+            else {
+                sRtn += "'" + obj[i].name + "',";
+            }
+        }
+        sRtn += "]";
+        return eval('(' + sRtn + ')');
+    }
+    my.getJsonValueByJosn = function (json) {
+        var obj = json;
+        var sRtn = "[";
+        for (var i = 0; i < obj.length; i++) {
+            if (i == obj.length - 1) {
+                sRtn += "" + obj[i].value + "";
+            }
+            else {
+                sRtn += "" + obj[i].value + ",";
+            }
+        }
+        sRtn += "]";
+        return eval('(' + sRtn + ')');
+    }
+    my.ajax = function (url, data, callback) {
+        var repeatCount = 1;
+        var currentRepeat = 0;
+        return $.ajax({
+            url: url,
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            data: data,
+            type: 'POST',
+            cache: false,
+            dataType: 'json'
+        }).done(function (json) {
+            if (json.Message) {
+                currentRepeat++;
+                if (currentRepeat > repeatCount)
+                    return my.ajax(url, data, ajaxdone, errcount)
+                else {
+                    var parentWin = window;
+                    while (parentWin && parentWin != parentWin.parent) {
+                        parentWin.__error__ = json.Message;
+                        parentWin = parentWin.parent;
+                    }
+                    parentWin.__error__ = json.Message;
+                    if (window.debug || window.location.host.indexOf('localhost') > -1)
+                        my.error(json.Message);
+                    my.error(json.Message);
+                    return;
+                }
+            }
+            callback && callback(json.result);
+
+        });
+    }
+
+    my.ajaxInvoke = function (method, data, callback, serverUrl) {      
+        if (!serverUrl) serverUrl = RoutePath;
+        if (method.indexOf('.') < 0) {
+            method = 'My.Application.' + method;
+        }       
+        return my.ajax(serverUrl + '?invokeMethod=' + method, { postdata: JSON.stringify(data) }, callback)
+
+    };
+
+    window.my = my;
+   
+    if (typeof module === 'object' && typeof module.exports === 'object') { module.exports = my; }
+    if (typeof angular === 'object') {
+        angular.module('api', []).factory('my', function () {
+            return my;
+        });
+    }
+    if (typeof define === 'function') { define(['jquery'], function ($) { return my; }); }
+
+    return my;
+   
+})(window)
+
+
+   
